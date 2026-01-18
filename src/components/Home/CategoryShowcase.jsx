c04f51243f9084dac55ac359163e349ead2c10c7
lvt17 <vtoanhihihi@gmail.com>
2026-01-18 15:20:26 +0700

    style: final fixes for homepage width alignment

--- src/components/Home/CategoryShowcase.jsx
+++ src/components/Home/CategoryShowcase.jsx
@@ -50,7 +50,7 @@ const CategoryShowcase = () => {
 
     return (
         <section className="py-16 bg-gray-50">
-            <div className="container mx-auto px-6 md:px-16 lg:px-24 max-w-6xl">
+            <div className="container mx-auto px-4 md:px-10 lg:px-20">
                 {/* Header */}
                 <motion.div
                     initial={{ y: 30, opacity: 0 }}
--- src/components/Home/VehicleShowcase.jsx
+++ src/components/Home/VehicleShowcase.jsx
@@ -87,7 +87,7 @@ const VehicleShowcase = () => {
 
     return (
         <section className="py-2 bg-gray-50">
-            <div className="container mx-auto px-6 md:px-16 lg:px-24 max-w-6xl">
+            <div className="container mx-auto px-4 md:px-10 lg:px-20">
                 {/* Header */}
                 <motion.div
                     initial={{ y: 20, opacity: 0 }}
